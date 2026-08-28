78db40085e655cf500ef77d4a72c2c1159764160
RainbowCodes <demetriuslb27@ausdk12.org>
2026-08-28 05:28:47 +0000

    ClassroomMain HW

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,5 +1,13 @@
 public class ClassroomMain {
     public static void main(String[] args) {
-        
+        Classroom r1 = new Classroom("Rasband", 30, 26);
+        Classroom m1 = new Classroom("Morris");
+
+        m1.setDesks(30);
+
+        System.out.println(
+            "r1 - Teacher: "+r1.teacher()+
+            " - Students: "+r1.students()+
+            " - Desks: "+r1.desks());
     }
 }
